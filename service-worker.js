8d4ecc592751032442d4bcd92f480d21f180e8bd
merterdem32 <merterdemkalyoncu@gmail.com>
2026-09-20 22:08:26 +0300

    Refresh cache for meeting notes fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ilke-saha-v6';
+const CACHE = 'ilke-saha-v7';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/styles.css', '/supabase-config.js', '/cloud.js'];
 
 self.addEventListener('install', event => {
