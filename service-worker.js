139a41be508c059c2b190e36f5be3d6dc833f604
merterdem32 <merterdemkalyoncu@gmail.com>
2026-09-20 20:55:50 +0300

    Bump service worker cache after fixes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ilke-saha-v2';
+const CACHE = 'ilke-saha-v3';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/styles.css'];
 
 self.addEventListener('install', event => {
