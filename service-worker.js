e891eef5121c3c39f10b9cdaca07a6a84519c922
merterdem32 <merterdemkalyoncu@gmail.com>
2026-09-25 13:48:02 +0300

    Bump service worker cache for dealer directions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ilke-saha-v44';
+const CACHE = 'ilke-saha-v45';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/styles.css', '/supabase-config.js', '/cloud.js', '/dealer-management.js', '/daily-report.js', '/ilke-logo.svg'];
 
 self.addEventListener('install', event => {
